a25227d212d1ed474b6e58dbfc688a99271ccaa9
Kerem Karabulut <ke.kerem3@gmail.com>
2021-01-23 21:10:56 +0300

    Add project files.

---
+++ MyDictionaryDemo/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, K>
+    {
+        T[] _array;
+        T[] _tempArray;
+        K[] _value;
+        K[] _tempValue;
+
+        public MyDictionary()
+        {
+            _array = new T[0];
+            _value = new K[0];
+        }
+
+        public void Add(T item1, K item2)
+        {
+            _tempArray = _array;
+            _tempValue = _value;
+
+            _array = new T[_array.Length + 1];
+            _value = new K[_value.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+                _value[i] = _tempValue[i];
+            }
+            _array[_array.Length - 1] = item1;
+            _value[_value.Length - 1] = item2;
+
+            Console.WriteLine(item1 + ".City" + ": " + item2 + " added in My Dictionary whit succesful.");
+        }
+    }
+}
---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Adana");
+            myDictionary.Add(2, "Adıyaman");
+            myDictionary.Add(3, "Afyon");
+            myDictionary.Add(4, "Ağrı");
+            myDictionary.Add(5, "Amasya");
+            myDictionary.Add(6, "Ankara");
+        }
+    }
+}
